5f8bcdabf34b74ffdc244abcaf9142eddd838b32
Timothy High <timothy.high@gmail.com>
2016-12-12 22:40:38 -0200

    Finished first database script and basic model structure

--- gruff/argument.go
+++ gruff/argument.go
@@ -1,13 +1,26 @@
 package gruff
 
+const ARGUMENT_TYPE_PRO_TRUTH int = 1
+const ARGUMENT_TYPE_CON_TRUTH int = 2
+const ARGUMENT_TYPE_PRO_RELEVANCE int = 3
+const ARGUMENT_TYPE_CON_RELEVANCE int = 4
+const ARGUMENT_TYPE_PRO_IMPACT int = 5
+const ARGUMENT_TYPE_CON_IMPACT int = 6
+
 type Argument struct {
 	Model
-	ParentID     uint64     `json:"parentId" sql:"not null"`
-	Parent       Debate     `json:"parent"`
+	UUID         string     `json:"uuid" sql:"not null"`
+	CreatedByID  uint64     `json:"createdById"`
+	CreatedBy    *User      `json:"createdBy"`
+	ParentID     *uint64    `json:"parentId,omitempty"`
+	Parent       *Debate    `json:"parent,omitempty"`
+	ArgumentID   *uint64    `json:"argumentId,omitempty"`
+	Argument     *Argument  `json:"argument,omitempty"`
 	DebateID     uint64     `json:"debateId" sql:"not null"`
 	Debate       Debate     `json:"debate"`
 	Title        string     `json:"title" sql:"not null" valid:"length(3|1000)"`
 	Description  string     `json:"desc" valid:"length(3|4000)"`
+	Type         int        `json:"type" sql:"not null"`
 	Relevance    float64    `json:"relevance"`
 	Impact       float64    `json:"impact"`
 	ProRelevance []Argument `json:"prorelev"`
--- gruff/link.go
+++ gruff/link.go
@@ -1,10 +1,13 @@
 package gruff
 
-type Reference struct {
+type Link struct {
 	Model
+	CreatedByID uint64  `json:"createdById"`
+	CreatedBy   *User   `json:"createdBy"`
+	UUID        string  `json:"uuid" sql:"not null"`
 	Title       string  `json:"title" sql:"not null" valid:"length(3|1000)"`
 	Description string  `json:"desc" valid:"length(3|4000)"`
-	Url         string  `json:"url" valid:"length(3|400)"`
+	Url         string  `json:"url" valid:"length(3|4000)"`
 	DebateID    *uint64 `json:"debateId"`
 	Debate      *Debate `json:"debate,omitempty"`
 }
